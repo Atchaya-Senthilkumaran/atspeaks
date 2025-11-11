6fe4701bd35513c11e75cf4f7ca3ea6e7247387e
Atchaya Senthilkumaran <atchayasenthilkumaran28@gmail.com>
2025-11-11 22:21:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const recordingRoutes = require('./routes/recordingRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || 'https://atspeaks.vercel.app/';
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 // Connect DB (non-blocking, will use mock data if fails)
 connectDB(process.env.MONGO_URI).catch(err => {
