243157a2562b2ffc528eb96a467b209b7ce8c43e
AtchayaS <atchayasenthilkumaran28@gmail.com>
2025-11-11 21:19:16 +0530

    updated env of server

--- server/server.js
+++ server/server.js
@@ -18,7 +18,10 @@ connectDB(process.env.MONGO_URI).catch(err => {
 });
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json({ limit: '5mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
