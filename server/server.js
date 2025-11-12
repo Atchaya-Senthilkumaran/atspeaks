dab28f055b48d79d687c1404acf5c8f2c23ad9a3
AtchayaS <atchayasenthilkumaran28@gmail.com>
2025-11-12 19:10:06 +0530

    clear error

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const connectDB = require('./config/db');
 
 const eventRoutes = require('./routes/eventRoutes');
@@ -29,7 +28,6 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // API routes
 app.use('/api/events', eventRoutes);
