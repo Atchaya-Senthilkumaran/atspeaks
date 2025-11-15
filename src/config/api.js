d9e29232ea0b508ad48908f91cbbb4b15f39d62b
AtchayaS <atchayasenthilkumaran28@gmail.com>
2025-11-15 19:57:12 +0530

    mobile resp3

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,5 @@
 // API Configuration - Production
-<<<<<<< HEAD
-const API_URL = 'https://atspeaksplease.vercel.app/';
-=======
 const API_URL = 'https://atspeaksplease.vercel.app';
->>>>>>> 41319b4ff545ae05cd91615fc5a3a901ef54eb45
 
 console.log('🔗 API URL configured as:', API_URL);
 
